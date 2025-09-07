03474b5ec20487fc6568f2213edbee4346d748dd
the-Piyush-Singh <2200521520036@ietlucknow.ac.in>
2025-09-07 10:19:29 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,8 +6,18 @@ export default defineConfig({
   server: {
     proxy: {
       "/ai": {
-        target: "http://localhost:3000", // your backend
+        target: "http://localhost:3000", // backend server
         changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  preview: {
+    proxy: {
+      "/ai": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
